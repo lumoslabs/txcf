8a0d74cf66d5337410b7b7772be8c0091d4c8b3d
Ivan Danci <bah4uk@gmail.com>
2020-01-16 18:40:34 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 1. Entry is published in contentful.
 2. Contentful sends webhook
 3. Service recieves contentful hook
-4. Push contentful_{entry_id}_en.json resource built from entry fields
+4. Push cf_{entry_name}_{entry_id}_en.json resource built from entry fields
 5. Transifex sends webhook
 6. Service recieves transifex hook
   6.1. fetch contentful entry
   6.2. replace fields with fields from transifex hook
-  6.3. push entry to contentful
+  6.3. push entry to contentful with relevant locale
   6.4. publish entry
--- script.rb
+++ script.rb
@@ -4,6 +4,7 @@ require 'transifex'
 require 'dotenv'
 
 Dotenv.load
+
 client = Contentful::Client.new(
   space: ENV['CONTENTFUL_SPACE'],  # This is the space ID. A space is like a project folder in Contentful terms
   access_token: ENV['CONTENTFUL_TOKEN']
@@ -17,8 +18,18 @@ end
 binding.pry
 # This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 entry = client.entry('EIo6nUr4D351f0Tu3tE4B', locale: 'en')
+# entry = client.entry('4z6FoIRMsE7QrUMfGGqysD', locale: 'en')
+# entry = client.entry('13lM9WJGDaBBL79WoqWrGR', locale: 'en')
 puts 11
 
 # transifex_project = Transifex::Project.new('lumos-static-site-generator')
 # transifex_project.languages.fetch
 # transifex_project.resources.fetch
+# project_resource = transifex_project.resource("enjson_1-heads_master")
+# project_resource.fetch_with_details
+# JSON.parse project_resource.content.fetch
+
+# resource_translation = project_resource.translation('en')
+# resource_translation_strings = resource_translation.strings
+# params = {:key => "welcome", :context => "", :translation => "new_translation"}
+# resource_translation_strings.update(params)
